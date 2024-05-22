5fd91137497248f4de04cf36ed4e44fb16551a53
dekim1028 <carat.kim@soomgo.com>
2024-05-22 13:00:23 +0900

    remove example

--- example/index.js
+++
@@ -1,5 +0,0 @@
-import { AppRegistry } from "react-native";
-import ReactNativeImageTransformerExample from "./ReactNativeImageTransformerExample";
-import { name as appName } from "./app.json";
-
-AppRegistry.registerComponent(appName, () => ReactNativeImageTransformerExample);
